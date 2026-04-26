b4dd2214ddad5bfef0b5d03ed511ac0a05ccf41a
CovidDoctor <nikvarybok@gmail.com>
2026-04-26 14:57:59 +0200

    Mechanization_0.1

--- Content.Shared/_GoobStation/CCVars/CCVars.Goob.cs
+++ Content.Shared/_GoobStation/CCVars/CCVars.Goob.cs
@@ -50,4 +50,14 @@ public sealed partial class GoobCVars
     /// </summary>
     public static readonly CVarDef<bool> SiloEnabled =
         CVarDef.Create("goob.silo_enabled", true, CVar.SERVER | CVar.REPLICATED);
+
+#region Mechs
+
+    /// <summary>
+    ///     Whether or not players can use mech guns outside of mechs.
+    /// </summary>
+    public static readonly CVarDef<bool> MechGunOutsideMech =
+    CVarDef.Create("mech.gun_outside_mech", true, CVar.SERVER | CVar.REPLICATED);
+
+#endregion
 }
